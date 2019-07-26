2c9c7307c6b561fd59d8715dc848c57134e5396c
Анна Михайлец <amile@amile-mbp.kvm-router>
2019-07-26 18:39:18 +0300

    Added easy slideshow

---
+++ app/blocks/slide/slideshow.js
@@ -0,0 +1,51 @@
+function Slideshow(element) {
+	this.slider = document.getElementsByClassName(element)[0];
+	this.slides = this.slider.getElementsByClassName('slide');
+	this.bullets = this.slider.getElementsByClassName('slideshow__nav-out');
+	this.bulletsIn = this.slider.getElementsByClassName('slideshow__nav-inner');
+	this.currentSlide = 0;
+	this.slideClassName = this.slides[this.currentSlide + 1].className;
+	this.slideClassNameActive = this.slides[this.currentSlide].className;
+	this.bulletClassName = this.bullets[this.currentSlide + 1].className;
+	this.bulletClassNameActive = this.bullets[this.currentSlide].className;
+	this.bulletInClassName = this.bulletsIn[this.currentSlide + 1].className;
+	this.bulletInClassNameActive = this.bulletsIn[this.currentSlide].className;
+	console.log(this.bulletClassName, this.bulletClassNameActive);
+	this.interval = null;
+}
+
+Slideshow.prototype.intervalSlideshow = function () {
+	this.interval = setInterval(this.nextSlide.bind(this), 4000);
+	this.userClick();
+};
+
+Slideshow.prototype.nextSlide = function () {
+	this.slides[this.currentSlide].className = this.slideClassName;
+	this.bullets[this.currentSlide].className = this.bulletClassName;
+	this.bulletsIn[this.currentSlide].className = this.bulletInClassName;
+	this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+	this.slides[this.currentSlide].className = this.slideClassNameActive;
+	this.bullets[this.currentSlide].className = this.bulletClassNameActive;
+	this.bulletsIn[this.currentSlide].className = this.bulletInClassNameActive;
+};
+
+Slideshow.prototype.userClick = function () {
+	const self = this;
+	for (let ind = 0; ind < self.bullets.length; ind++) {
+		self.bullets[ind].addEventListener('click', function () {
+			clearInterval(self.interval);
+			self.slides[self.currentSlide].className = self.slideClassName;
+			self.bullets[self.currentSlide].className = self.bulletClassName;
+			self.bulletsIn[self.currentSlide].className = self.bulletInClassName;
+			self.slides[ind].className = self.slideClassNameActive;
+			self.bullets[ind].className = self.bulletClassNameActive;
+			self.bulletsIn[ind].className = self.bulletInClassNameActive;
+			self.currentSlide = ind;
+			self.interval = setInterval(self.nextSlide.bind(self), 4000);
+		});
+	}
+};
+
+const slider = new Slideshow('slideshow');
+
+slider.intervalSlideshow();
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,8 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
+import '../blocks/slide/slideshow.js';
+
 $(() => {
 	svg4everybody();
 });
